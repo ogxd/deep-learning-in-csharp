ef50bf0036e66071e958117773684280164dba01
Olivier Giniaux <oginiaux@live.com>
2017-12-10 20:44:47 +0100

    Add LiveCharts for realtime data visualization in WPF

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# **Derp** - **De**ep learning in C sha**rp** !
+
+**Derp** is a Neural Network generation and training framework written in C#.
+It works entirely with builtin arrays and term by term calculations, resulting in very good performances connsidering it is entirely CPU based.
+
+This library was written for my own learning of machine learning topics.
---
+++ StockPrediction/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StockPrediction {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+
+        public MainWindow() {
+            InitializeComponent();
+
+            SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Series 1",
+                    Values = new ChartValues<double> { 4, 6, 5, 2 ,7 },
+                    Stroke = Brushes.Red,
+                },
+                new LineSeries
+                {
+                    Title = "Series 2",
+                    Values = new ChartValues<double> { 6, 7, 3, 4 ,6 }
+                }
+            };
+
+            Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
+            YFormatter = value => value.ToString("C");
+
+            //modifying the series collection will animate and update the chart
+            SeriesCollection.Add(new LineSeries {
+                Values = new ChartValues<double> { 5, 3, 2, 4 },
+                LineSmoothness = 0 //straight lines, 1 really smooth lines
+            });
+
+            //modifying any series values will also animate and update the chart
+            SeriesCollection[2].Values.Add(5d);
+
+            DataContext = this;
+        }
+    }
+}
